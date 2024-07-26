d2038ba96232132758cf56024b73433b0cbbc288
Iluze Ear <nsumarna150@gmail.com>
2024-07-26 17:38:40 +0700

    Manambahkan CSS

--- 24Juli24/javaScript.js
+++ 24Juli24/javaScript.js
@@ -1,27 +1,62 @@
-function person(firstName, lastName, hobies, email, address) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.hobies = hobies;
-  this.email = email;
-  this.address = address;
-  this.fullName = function () {
-    return `${this.firstName} ${this.lastName}`;
-  };
-  this.leanguage = "Indonesia";
+// function person(firstName, lastName, hobies, email, address) {
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+//   this.hobies = hobies;
+//   this.email = email;
+//   this.address = address;
+//   this.fullName = function () {
+//     return `${this.firstName} ${this.lastName}`;
+//   };
+//   this.leanguage = "Indonesia";
+// }
+
+// const diki = new person(
+//   "Dicky",
+//   "Darmansyah",
+//   "Code, Binaraga",
+//   "DikiDarmansyah15@gmail.com",
+//   "Tasikmalaya - Jamanis"
+// );
+
+// const adit = new person(
+//   "Aditya",
+//   "Ardinsyah",
+//   "Melukis",
+//   "aditya@gmail.com",
+//   "Tasikmalaya-Jamanis"
+// );
+
+// const person = {
+//   firstName: prompt("Masukkan nama"),
+//   lastName: prompt("Masukkan nama Belakang"),
+//   hobies: prompt("Masukkan data Hobi"),
+//   fullName: function () {
+//     return `${this.firstName} ${this.lastName}`;
+//   },
+// };
+
+// alert(person.fullName());
+
+const dataClass = {
+  satu: calssUp.adit,
+};
+function calssUp(firstName, lastName, leanguge, Dream, hobies, address) {
+  (this.firstName = firstName),
+    (this.lastName = lastName),
+    (this.fullName = function () {
+      return `${this.firstName} ${this.lastName}`;
+    });
+  (this.leanguge = leanguge),
+    (this.Dream = Dream),
+    (this.hobies = hobies),
+    (this.address = address);
 }
 
-const diki = new person(
-  "Dicky",
-  "Darmansyah",
-  "Code, Binaraga",
-  "DikiDarmansyah15@gmail.com",
-  "Tasikmalaya - Jamanis"
-);
-
-const adit = new person(
+const adit = new calssUp(
   "Aditya",
-  "Ardinsyah",
-  "Melukis",
-  "aditya@gmail.com",
+  "Ardiansyah",
+  "Indonesia, sunda",
+  "Engineering",
+  "Art",
   "Tasikmalaya-Jamanis"
 );
---
+++ Play Dik/script.js
@@ -0,0 +1,14 @@
+let Pasangan = 5;
+let Crush = 20;
+let calonPasanganDiki = Pasangan + Crush;
+// +, -, /, *, **, %, ++, --
+
+let x = 20;
+let y = 6;
+let z = x % y;
+
+let Kamu = "Diki "
+let Dia = " Melly "
+let pasangan = Kamu + Dia + " Saling mencintai"
+
+alert(pasangan);
